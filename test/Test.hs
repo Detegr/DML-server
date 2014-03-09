507327c148e67b51b9ddb151ef5a51b661ace4a5
Antti Keränen <detegr@gmail.com>
2014-03-09 21:31:16 +0200

    supply test

--- test/Test.hs
+++ test/Test.hs
@@ -9,10 +9,28 @@ import DML.Game
 resourceGen :: Gen Resource
 resourceGen = elements [Slave, Spice, Iron, Wood, Joker]
 
+isKing :: Card -> Bool
+isKing (King _) = True
+isKing _        = False
+
+charOrGuild :: Card -> Bool
+charOrGuild (Character _) = True
+charOrGuild (Guild _)     = True
+charOrGuild _             = False
+
+cardGenNoKing :: Gen Card
+cardGenNoKing = arbitrary `suchThat` (not . isKing)
+
+cardGenKing :: Gen Card
+cardGenKing = arbitrary `suchThat` isKing
+
+cardGenCharOrGuild :: Gen Card
+cardGenCharOrGuild = arbitrary `suchThat` charOrGuild
+
 instance Arbitrary Card where
   arbitrary = do
     resource <- resourceGen
-    value <- choose (1,12) -- Do not generate kings
+    value <- choose (1,13)
     case mkCard resource value of
       Nothing -> arbitrary
       Just card -> return card
@@ -23,27 +41,30 @@ instance Arbitrary Market where
     l2 <- choose(0,54-l1)
     l3 <- choose(0,54-l1+l2)
     l4 <- choose(0,54-l1+l2+l3)
-    m1 <- vector l1
-    m2 <- vector l2
-    m3 <- vector l3
-    m4 <- vector l4
+    m1 <- vectorOf l1 cardGenNoKing
+    m2 <- vectorOf l2 cardGenNoKing
+    m3 <- vectorOf l3 cardGenNoKing
+    m4 <- vectorOf l4 cardGenNoKing
     return $ Market m1 m2 m3 m4
 
 instance Arbitrary DMLState where
   arbitrary = do
-    len <- choose (0,54)
-    len2 <- choose (0,54-len)
-    len3 <- choose (0,54-len+len2)
-    deck <- vector len
-    market <- (arbitrary :: Gen Market)
-    bMarket <- vector len2
-    loot <- vector len3
+    deck <- listOf cardGenNoKing
+    market <- arbitrary
+    bm1 <- listOf cardGenNoKing
+    bm2 <- listOf cardGenNoKing
+    bm3 <- listOf cardGenNoKing
+    loot <- listOf cardGenNoKing
     pv <- vector 4
     let players = (pv !! 0, pv !! 1, pv !! 2, pv !! 3)
-    return $ DMLState deck market bMarket loot players Nothing
+    return $ DMLState deck market [bm1,bm2,bm3] loot players Nothing
 
 instance Arbitrary Player where
-  arbitrary = return $ Player [] [] (fromJust $ mkCard Slave 13)
+  arbitrary = do
+    king <- cardGenKing
+    treasury <- listOf cardGenNoKing
+    house <- listOf cardGenCharOrGuild
+    return $ Player [] [] king
 
 prop_draw :: DMLState -> Bool
 prop_draw s = length drawnDeck == (length fullDeck) - 1 || (length fullDeck) == 0
@@ -51,5 +72,41 @@ prop_draw s = length drawnDeck == (length fullDeck) - 1 || (length fullDeck) == 
         fullDeck = s ^. deck
         drawnDeck = s' ^. deck
 
+prop_supply :: DMLState -> Bool
+prop_supply s = case card of
+  Just c ->
+    case c of
+      DragonEgg _ -> checkLoot
+      Character _ -> checkLoot
+      Guild     _ -> checkLoot
+      Card    r _ -> checkMarket r
+      Thief       -> checkLoot
+  Nothing ->
+    length slaveMarket == length slaveMarket' &&
+    length spiceMarket == length spiceMarket' &&
+    length ironMarket  == length ironMarket'  &&
+    length woodMarket  == length woodMarket'  &&
+    length dragonsLoot == length dragonsLoot'
+  where s' = supply s
+        (card,_) = draw s
+        slaveMarket = s ^. market . slaves
+        slaveMarket' = s' ^. market . slaves
+        spiceMarket = s ^. market . spices
+        spiceMarket' = s' ^. market . spices
+        ironMarket = s ^. market . iron
+        ironMarket' = s' ^. market . iron
+        woodMarket = s ^. market . wood
+        woodMarket' = s' ^. market . wood
+        dragonsLoot = s ^. loot
+        dragonsLoot' = s' ^. loot
+        checkPlusOne a b = length a == length b + 1
+        checkLoot = checkPlusOne dragonsLoot' dragonsLoot
+        checkMarket r =
+          case r of
+            Slave -> checkPlusOne slaveMarket' slaveMarket
+            Spice -> checkPlusOne spiceMarket' spiceMarket
+            Iron  -> checkPlusOne ironMarket' ironMarket
+            Wood  -> checkPlusOne woodMarket' woodMarket
+
 main :: IO()
-main = quickCheck prop_draw
+main = mapM_ quickCheck [prop_draw, prop_supply]
